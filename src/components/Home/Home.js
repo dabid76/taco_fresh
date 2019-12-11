a563daf21610f1d330efc564984588bd9440769e
David Diaz <daemdisa@aol.com>
2019-12-11 13:49:15 -0600

    seeing how i like all the pictures in the landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { Header, Icon } from 'semantic-ui-react'
 import './Home.css'
+import A0 from '../images/A0.jpg'
 import A1 from '../images/A1.jpg'
+import A2 from '../images/A2.jpg'
+import A3 from '../images/A3.jpg'
+import A4 from '../images/A4.jpg'
+
+import T1 from '../images/T1.jpg'
+import T2 from '../images/T2.jpg'
+import T3 from '../images/T3.jpg'
+import T4 from '../images/T4.jpg'
+import T5 from '../images/T5.jpg'
+
+
 
 class Home extends Component {
     menu = (propertyName) =>{
@@ -30,8 +42,22 @@ class Home extends Component {
             </p>
         {/* <div className="grid-container"> */}
         <main className="flexbox">
-            <p className="grid-item" onClick={(event) => this.menu('Tacos', event)}><img src={A1} height='100vh' width='90%' />Tacos Menu</p>
-            <p className="grid-item" onClick={(event) => this.menu('Rolls', event)}>Rolls</p>
+            <div className="grid-item" onClick={(event) => this.menu('Tacos', event)}>
+                <img src={A0} width="90%" height="100vh" />
+                <img src={A1} width="90%" height="100vh" />
+                <img src={A2} width="90%" height="100vh" />
+                <img src={A3} width="90%" height="100vh" />
+                <img src={A4} width="90%" height="100vh" />
+                Tacos
+            </div>
+            <div className="grid-item" onClick={(event) => this.menu('Rolls', event)}>
+                <img src={T1} width="90%" height="100vh" />
+                <img src={T2} width="90%" height="100vh" />
+                <img src={T3} width="90%" height="100vh" />
+                <img src={T4} width="90%" height="100vh" />
+                <img src={T5} width="90%" height="100vh" />
+                Rolls
+            </div>
             <p className="grid-item" onClick={(event) => this.menu('Appetizers', event)}>Appetizers</p>
             <p className="grid-item" onClick={(event) => this.menu('Drinks', event)}>Drinks</p>
         </main>
